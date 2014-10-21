acd4142a315d75b9181b7aaf005b8c16c3677ef8
Edwin Joassart <edwin@3kd.be>
2014-10-21 17:17:01 +0200

    remove half of small lines

--- geo2gcode.js
+++ geo2gcode.js
@@ -1,7 +1,8 @@
 var settings = {
   zOn: 255,
   zOff: 0,
-  feedrate: 2000
+  feedrate: 2000,
+  thresh: 5
 }
 var gcode =[];
 
@@ -15,10 +16,27 @@ for(var f=0; f < json["features"].length; f++){
           gcode.push("G00 F" + settings.feedrate + " Z" + settings.zOff); //turn off to prepare seek
           for(var poly=0; poly<geo["coordinates"][spoly].length; poly++)
           {
+            var X = geo["coordinates"][spoly][poly][0];
+            var Y = geo["coordinates"][spoly][poly][1];
+
             if(poly==1){//turn on to draw all the next lines
               gcode.push("G00 F" + settings.feedrate + " Z" + settings.zOn); //turn off to prepare seek
             }
-            gcode.push("G00 F"+settings.feedrate+" X"+geo["coordinates"][spoly][poly][0]+" Y"+geo["coordinates"][spoly][poly][1]); // push lines
+
+            if(poly < geo["coordinates"][spoly].length - 1) //are we not at the end of path ?
+            {
+              var X2 = geo["coordinates"][spoly][poly+1][0];
+              var Y2 = geo["coordinates"][spoly][poly+1][1];
+
+              if(Math.abs(X2 - X) <= settings.thresh && Math.abs(Y2-Y) <= settings.thresh)
+              {
+                X = X2;
+                Y = Y2;
+                poly = poly+1;
+              }
+            }
+
+            gcode.push("G00 F" + settings.feedrate + " X" + X + " Y" + Y); // push lines
           }
         }
       break;
@@ -27,3 +45,5 @@ for(var f=0; f < json["features"].length; f++){
 }
 
 console.log(gcode.join("\n"));
+
+//console.log("LENGTH : " + gcode.length);
