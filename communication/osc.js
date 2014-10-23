90eb50e701cd7f27401d8ad8a668781bc5a2f79d
Edwin Joassart <edwin@3kd.be>
2014-10-23 04:40:55 +0200

    osc

--- communication/osc.js
+++ communication/osc.js
@@ -2,7 +2,7 @@ var root = require('../root'),
   fs = require('fs'),
   util = require('util'),
   events = require('events'),
-  osc = require("node-osc");
+  osc = require("node-osc"),
 
 /**
  * Osc class to talk with the ofx code
@@ -10,7 +10,7 @@ var root = require('../root'),
 var OSC = function() {
   var self = this;
 
-  var sendEOD = function()
+  this.sendEOD = function()
   {
     var client = new osc.Client('127.0.0.1', '3333');
     client.send('/EOD', 'true')
