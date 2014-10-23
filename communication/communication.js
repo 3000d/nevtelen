f59a02253fad854451158be785b64cc3a721da73
john <john@3kd.be>
2014-10-23 03:12:35 +0200

    bugfix too many eventemitters

--- communication/communication.js
+++ communication/communication.js
@@ -80,12 +80,12 @@ var Communication = function() {
               firstArrow = false;
             }
           }
+        });
 
-          serial.on('error', function(error){
-            self.emit(self.EVENT.DISCONNECTED);
-            self.Log.error('ERROR + serial error - disconnected \n' + error);
-            isConnected = false;
-          });
+        serial.on('error', function(error){
+          self.emit(self.EVENT.DISCONNECTED);
+          self.Log.error('ERROR + serial error - disconnected \n' + error);
+          isConnected = false;
         });
       });
     }
