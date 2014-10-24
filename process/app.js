c23aafd94f4777628588e584f30cb5619f78247d
Edwin Joassart <edwin@3kd.be>
2014-10-24 17:16:51 +0200

    tuning compare

--- process/app.js
+++ process/app.js
@@ -67,7 +67,7 @@ drawbot.getSerialPortList(function(ports) {
         return;
       }else
       {
-        if(stdout.split(' ')[0] > 1400)
+        if(stdout.split(' ')[0] > 1250)
         {
           drawbot.log('compare : got it ' + stdout[0]);
           exec(potrace, function(error, stdout, stderr) {
