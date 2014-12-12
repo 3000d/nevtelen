a6f2edb8ad1420dce77aa1739e2dd3d434da9f6d
edwin <edwin@3kd.be>
2014-12-12 19:06:30 +0100

    debug

--- process/app.js
+++ process/app.js
@@ -87,9 +87,12 @@ drawbot.getSerialPortList(function(ports) {
           {
             drawbot.Log.debug('gotit' + strerr.split(' ')[0]);
             exec(crop, function(error, stdout, strerr){
-              drawbot.Log.error(crop);
-              drawbot.Log.error("error " + error);
-              drawbot.Log.error("crop " + strerr);
+              if(error && error !== 'null')
+              {
+                drawbot.Log.error(crop);
+                drawbot.Log.error("error " + error);
+                drawbot.Log.error("crop " + strerr);
+              }
               exec(potrace, function(error, stdout, stderr) {
                 if((error && error !== 'null') || stderr) {
                   drawbot.Log.error('potrace ' + error);
