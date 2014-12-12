d45acc8e91c3bc4a31efbea687f2b1c89ec519c2
john <john@3kd.be>
2014-12-12 19:30:54 +0100

    removed crop dezoom

--- process/app.js
+++ process/app.js
@@ -68,7 +68,7 @@ drawbot.getSerialPortList(function(ports) {
     //var modw = w/10;
     //var modh = h/10;
     //var crop = 'convert -crop ' + (w+modw) + 'x' + (h+modh) + '+' + (x-modw/2) + '+' + (y-modh/2) + ' ' + evt.path + ' ' + evt.path;
-    var crop = 'convert -crop ' + (w) + 'x' + (h) + ' ' + evt.path + ' ' + evt.path;
+    var crop = 'convert -crop ' + (w) + 'x' + (h) + '+' + (x) + '+' + (y) + ' ' + evt.path + ' ' + evt.path;
 
     exec(compare, function(error, stdout, strerr){
       if((error && error !== 'null'))
